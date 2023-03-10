4c0aab67e29764d7dbae1611398d1beebe19bb24
Kush Behl <behlkush@gmail.com>
2023-03-10 21:35:08 +0000

    Sample text added in home activities

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -55,8 +55,8 @@ class HomeActivities:
       if cognito_user_id != None:
         extra_crud = {
           'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Lore',
-          'message': 'My dear brother, it the humans that are the problem',
+          'handle':  'Lore-KB',
+          'message': 'My dear brother, it the humans that are the problem: BY KB',
           'created_at': (now - timedelta(hours=1)).isoformat(),
           'expires_at': (now + timedelta(hours=12)).isoformat(),
           'likes': 1042,
