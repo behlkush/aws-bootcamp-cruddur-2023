eb284d91a650522ea3d5b6e1a8fc52b9ef054143
Kush Behl <behlkush@gmail.com>
2023-03-21 17:51:20 +0000

    updated missing user handle

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -224,6 +224,7 @@ def data_search():
 @cross_origin()
 def data_activities():
     #user_handle = 'owensound'
+    user_handle =  request.json["user_handle"]
     message = request.json['message']
     ttl = request.json['ttl']
     model = CreateActivity.run(message, user_handle, ttl)
