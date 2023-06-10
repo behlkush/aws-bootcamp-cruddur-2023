602a8d1c63b5cf4998ef3769469838a5bac61eda
Kush Behl <behlkush@gmail.com>
2023-06-10 03:26:32 +0000

    Fixed s3 bucket name and dynamic Origin for cruddur upload lambda

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -4,13 +4,19 @@ require 'jwt'
 
 def handler(event:, context:)
   puts event
+  origin = event["headers"]["origin"]
+  puts("origin", origin)
+  dev_url = "https:\/\/3000-behlkush-awsbootcampcru-(.+)\.gitpod\.io"
+  if /#{dev_url}/.match(origin).nil? # if no match return the prod domain
+    origin = "https://gsdcanadacorp.info"
+  end
   # Return CORS headers for pre-flight check
   if event['routeKey'] == "OPTIONS /{proxy+}"
     puts({step: 'preflight', message: 'preflight CORS check'}.to_json)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://3000-behlkush-awsbootcampcru-7u7e08bt761.ws-us99.gitpod.io",
+        "Access-Control-Allow-Origin": origin,
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200
@@ -36,7 +42,7 @@ def handler(event:, context:)
     { 
       headers: {
           "Access-Control-Allow-Headers": "*, Authorization",
-          "Access-Control-Allow-Origin": "https://3000-behlkush-awsbootcampcru-7u7e08bt761.ws-us99.gitpod.io",
+          "Access-Control-Allow-Origin": origin,
           "Access-Control-Allow-Methods": "OPTIONS, GET, POST"
       },
       statusCode: 200, 
@@ -44,55 +50,3 @@ def handler(event:, context:)
     }
   end # if 
 end # def handler
-
-# puts handler(
-#   event: {},
-#   context: {}
-# )
-# require 'json'
-# require 'jwt'
-
-# def handler(event:, context:)
-#   puts event
-#   # return cors headers for preflight check
-#   if event['routeKey'] == "OPTIONS /{proxy+}"
-#     puts({step: 'preflight', message: 'preflight CORS check'}.to_json)
-#     { 
-#       headers: {
-#         "Access-Control-Allow-Headers": "*, Authorization",
-#         "Access-Control-Allow-Origin": "https://3000-omenking-awsbootcampcru-2n1d6e0bd1f.ws-us94.gitpod.io",
-#         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
-#       },
-#       statusCode: 200
-#     }
-#   else
-#     token = event['headers']['authorization'].split(' ')[1]
-#     puts({step: 'presignedurl', access_token: token}.to_json)
-
-#     body_hash = JSON.parse(event["body"])
-#     extension = body_hash["extension"]
-
-#     decoded_token = JWT.decode token, nil, false
-#     cognito_user_uuid = decoded_token[0]['sub']
-
-#     s3 = Aws::S3::Resource.new
-#     bucket_name = ENV["UPLOADS_BUCKET_NAME"]
-#     object_key = "#{cognito_user_uuid}.#{extension}"
-
-#     puts({object_key: object_key}.to_json)
-
-#     obj = s3.bucket(bucket_name).object(object_key)
-#     url = obj.presigned_url(:put, expires_in: 60 * 5)
-#     url # this is the data that will be returned
-#     body = {url: url}.to_json
-#     { 
-#       headers: {
-#         "Access-Control-Allow-Headers": "*, Authorization",
-#         "Access-Control-Allow-Origin": "https://3000-omenking-awsbootcampcru-2n1d6e0bd1f.ws-us94.gitpod.io",
-#         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
-#       },
-#       statusCode: 200, 
-#       body: body 
-#     }
-#   end # if 
-# end # def handler
